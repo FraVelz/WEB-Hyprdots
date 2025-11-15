23f109de97928c96fea43c4f0920151827a94276
Fravelz <fravelz_@hotmail.com>
2025-11-15 13:18:08 -0500

    Actualización: 0.0.1

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,25 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
+ plugins: [
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
       },
     }),
   ],
-})
+
+  base: process.env.NODE_ENV === 'production' ? '/WEB-Notas-de-Hacking/' : '/',
+
+  server: {
+    historyApiFallback: true
+  },
+
+   build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined, // Vite se encargará de dividir automáticamente
+      },
+    },
+  },
+});
